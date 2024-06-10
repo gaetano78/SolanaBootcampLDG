d7e0d6fd88d3fef1c706fb8e9670b258fbcd9463
gaetano78 <g.la.delfa@alice.it>
2024-06-10 18:28:55 +0200

    Add files via upload

--- keygen.ts
+++ keygen.ts
@@ -1,11 +1,10 @@
 //In questo codice sto semplicemente generando una keypair per il wallet
-
 import { Keypair, PublicKey } from "@solana/web3.js"; 
 
 //generiamo una nuova keypair
 const keypair = Keypair.generate();
 
-//la stampo
+//stampiamo chiave pubblica e privata
 console.log("PublicKey = ", keypair.publicKey.toBase58());
 console.log("PrivateKey = ", keypair.secretKey.toString())
 
